3b054c8257b654643babbe80b3712367a7b4e6dc
Dino Leung <dino.leung@enabled.com.au>
2024-11-28 22:48:49 +1030

    adding service uuids

---
+++ composeApp/src/commonMain/kotlin/xyz/d1n0/lib/BleUuid.kt
@@ -0,0 +1,10 @@
+package xyz.d1n0.lib
+
+import com.benasher44.uuid.uuidFrom
+
+object BlsUuid {
+    val CASIO_SERVICE_UUID = uuidFrom("00001804-0000-1000-8000-00805f9b34fb")
+    val ALL_FEATURES_CHARACTERISTIC_UUID = uuidFrom("26eb000d-b012-49a8-b1f8-394fb2032b0f")
+    val REQUEST_CHARACTERISTIC_UUID = uuidFrom("26eb002c-b012-49a8-b1f8-394fb2032b0f")
+    val IO_CHARACTERISTIC_UUID = uuidFrom("26eb002d-b012-49a8-b1f8-394fb2032b0f")
+}
