a41e405b923773e3b9e6feb680570712919d47cd
Dino Leung <dino.ph.leung@gmail.com>
2025-03-19 21:54:39 +1030

    cleanup

--- composeApp/src/commonMain/kotlin/xyz/d1n0/view/ScanScreen.kt
+++ composeApp/src/commonMain/kotlin/xyz/d1n0/view/ScanScreen.kt
@@ -4,18 +4,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
-import xyz.d1n0.model.Watch
 import xyz.d1n0.viewModel.ScanScreenViewModel
 
 @Composable
