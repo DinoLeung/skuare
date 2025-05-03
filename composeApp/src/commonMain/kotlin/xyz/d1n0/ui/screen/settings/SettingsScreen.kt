75c3b7046ff12362f6d76bfea1697a4ee5926ef3
Dino Leung <dino.ph.leung@gmail.com>
2025-05-03 19:47:33 +0930

    implementing settings screen

--- composeApp/src/commonMain/kotlin/xyz/d1n0/ui/screen/settings/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/xyz/d1n0/ui/screen/settings/SettingsScreen.kt
@@ -6,13 +6,18 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -23,17 +28,40 @@ fun SettingsScreen(
 	val state by viewModel.uiState.collectAsState()
 
 	LaunchedEffect(Unit) {
-		if (state.isNameInitialized == null) viewModel.onEvent(SettingsUiEvent.RequestName)
-		if (state.isWatchSettingsInitialized == null) viewModel.onEvent(SettingsUiEvent.RequestWatchSettings)
-		if (state.isConnectionSettingsInitialized == null) viewModel.onEvent(SettingsUiEvent.RequestConnectionSettings)
+		if (state.isNameInitialized == false) viewModel.onEvent(SettingsUiEvent.RequestName)
+		if (state.isWatchSettingsInitialized == false) viewModel.onEvent(SettingsUiEvent.RequestWatchSettings)
+		if (state.isConnectionSettingsInitialized == false) viewModel.onEvent(SettingsUiEvent.RequestConnectionSettings)
 	}
+
+	var nameFieldValue by remember { mutableStateOf(TextFieldValue(text = state.savedName.value)) }
+	LaunchedEffect(state.savedName) {
+		nameFieldValue = nameFieldValue.copy(state.savedName.value)
+	}
+
 
 	Column(
 		modifier = Modifier.fillMaxSize().padding(innerPadding),
 		horizontalAlignment = Alignment.CenterHorizontally,
 		verticalArrangement = Arrangement.Center,
 	) {
-		Text(state.savedName.value, color = MaterialTheme.colorScheme.onBackground)
+		OutlinedTextField(
+			label = { Text("Name") },
+			value = nameFieldValue,
+			enabled = state.waitingUpdates != true,
+			isError = state.pendingNameError != null,
+			supportingText = {
+				state.pendingNameError?.let {
+					Text(
+						it.message ?: "Unknown errors"
+					)
+				}
+			},
+			onValueChange = {
+				nameFieldValue = it
+				viewModel.onEvent(SettingsUiEvent.NameInputChange(it.text))
+			}
+		)
+//		Text(state.savedName.value, color = MaterialTheme.colorScheme.onBackground)
 		Text(state.savedWatchSettings.toString(), color = MaterialTheme.colorScheme.onBackground)
 		Text(
 			state.savedConnectionSettings.toString(),
