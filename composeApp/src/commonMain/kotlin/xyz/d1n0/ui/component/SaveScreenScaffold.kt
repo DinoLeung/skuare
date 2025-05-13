a987b8579d0c9aaed45a62c095df1331a4ed4fa7
Dino Leung <dino.ph.leung@gmail.com>
2025-05-13 22:04:50 +0930

    fixes timmer screen fav position

--- composeApp/src/commonMain/kotlin/xyz/d1n0/ui/component/SaveScreenScaffold.kt
+++ composeApp/src/commonMain/kotlin/xyz/d1n0/ui/component/SaveScreenScaffold.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Save
@@ -21,7 +22,7 @@ fun SaveScreenScaffold(
 	saveFabOnClick: () -> Unit,
 	content: @Composable () -> Unit,
 ) {
-	Box(modifier = modifier) {
+	Box(modifier = modifier.fillMaxSize()) {
 		content()
 		AnimatedVisibility(
 			visible = saveFabVisible,
